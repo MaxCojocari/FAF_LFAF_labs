aed757995e6c71cd3aafc60e40a5168b1ef84745
MaxCojocari <cojocarigoncearmax@gmail.com>
2023-02-11 12:02:17 +0200

    configure files

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,4 @@
-export const sum = (a: number, b: number) => {
-  if ('development' === process.env.NODE_ENV) {
-    console.log('boop');
-  }
-  return a + b;
-};
+export * from "./automation/FiniteAutomaton";
+export * from "./automation/Transition";
+export * from "./grammar/Grammar";
+export * from "./grammar/Production";
